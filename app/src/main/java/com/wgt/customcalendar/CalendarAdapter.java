67d3f73518c2ae8e110dcacfa7bf87a9c19fd701
KaiserPerwez <jobs4kaiser@gmail.com>
2018-03-14 15:18:30 +0530

    Merged

--- app/src/main/java/com/wgt/customcalendar/CalendarAdapter.java
+++ app/src/main/java/com/wgt/customcalendar/CalendarAdapter.java
@@ -1,7 +1,7 @@
 package com.wgt.customcalendar;
 
 import android.content.Context;
-import android.util.Log;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,34 +21,15 @@ public class CalendarAdapter extends BaseAdapter {
     ViewHolder holder;
     LayoutInflater inflater;
     Context context;
-    int cal_startDate = 1;
-    int cal_startMax = 31;
-    int cal_firstDayNumOfWeek;
-    int num_of_days_in_month;
 
     public CalendarAdapter(Context context, Date currentDate) {
         this.context = context;
         this.currentDate = currentDate;
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        cal_firstDayNumOfWeek = getFirstDayNumOfWeek();
-        //num_of_days_in_month = getEndWeekDayNum();
     }
-
-    private int getFirstDayNumOfWeek() {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(currentDate);
-        cal.set(Calendar.DAY_OF_MONTH, 1);
-        return cal.get(Calendar.DAY_OF_WEEK);
+    public void nextMonth(Date date){
+        this.currentDate=date;
     }
-
-   /* private int getEndWeekDayNum() {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(currentDate);
-        int i = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-        return i;
-    }*/
-
     @Override
     public int getCount() {
         return (6 * 7);//6 weeks
@@ -65,13 +46,12 @@ public class CalendarAdapter extends BaseAdapter {
     }
 
     int i = 0;
-    StringBuilder sb;
-
+    Calendar cal = Calendar.getInstance();
+    String dt;
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
+        // date = new Date(currentDate.getTime());
         holder = new ViewHolder();
-        sb = new StringBuilder();
         View itemView = inflater.inflate(R.layout.grid_item, null);
         holder.ll = itemView.findViewById(R.id.grid_item);
        /* if (i<cal_firstDayNumOfWeek || i>=num_of_days_in_month){
@@ -85,22 +65,16 @@ public class CalendarAdapter extends BaseAdapter {
 
         holder.tv = itemView.findViewById(R.id.txt_date);
         if (i++ < 6)
-            sb.append("i");
+            dt="i";
         else {
-            if (cal_startDate <= cal_startMax)
-                sb = sb.append(cal_startDate++);
-            else if (cal_startDate > cal_startMax) {
-                int diff = cal_startDate++ - cal_startMax;
-                if (diff > num_of_days_in_month) {
-                    sb.append((diff - num_of_days_in_month));
-                } else
-                    sb.append(diff);
-            }
-            Log.d("SB: ", sb.toString());
+            dt = (String) DateFormat.format("dd", currentDate);
+            cal.setTime(currentDate);
+            cal.add(Calendar.DATE, 1);
+            currentDate = cal.getTime();
         }
         //holder.tv.setText(sb.append(" ").append(cal_startDate));
 
-        holder.tv.setText(sb);
+        holder.tv.setText(dt);
         return itemView;
     }
 
--- app/src/main/java/com/wgt/customcalendar/MainActivity.java
+++ app/src/main/java/com/wgt/customcalendar/MainActivity.java
@@ -2,6 +2,8 @@ package com.wgt.customcalendar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
+import android.view.View;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private ImageView btnNext;
     private TextView txtDate;
     private GridView grid;
+    Date startDate = null;
+    String month = null;
+    CalendarAdapter adapter;
+    boolean firstClick = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +38,55 @@ public class MainActivity extends AppCompatActivity {
         grid = (GridView) findViewById(R.id.calendar_grid);
 
         String startDate_str = "26/02/2018";
-        Date startDate = null;
         try {
             startDate = new SimpleDateFormat("dd/MM/yyyy").parse(startDate_str);
+            txtDate.setText(DateFormat.format("MMM-yyyy", startDate));
         } catch (ParseException e) {
             e.printStackTrace();
         }
         if (startDate != null) {
-            CalendarAdapter adapter = new CalendarAdapter(this, startDate);
+            adapter = new CalendarAdapter(this, startDate);
             grid.setAdapter(adapter);
-            Toast.makeText(this, "Date: "+startDate, Toast.LENGTH_SHORT).show();
+            /*btnNext.performClick();
+            btnPrev.performClick();*/
+            Toast.makeText(this, "Date: " + startDate, Toast.LENGTH_SHORT).show();
         } else
             Toast.makeText(this, "Date couldn't be parsed", Toast.LENGTH_SHORT).show();
+
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(startDate);
+
+                if (firstClick) {
+                    cal.add(Calendar.DATE, 39);
+                    firstClick = false;
+                } else
+                    cal.add(Calendar.DATE, 42);
+
+                adapter.nextMonth(cal.getTime());
+                adapter.notifyDataSetChanged();
+                startDate = cal.getTime();
+            }
+        });
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(startDate);
+
+                if (firstClick) {
+                    cal.add(Calendar.DATE, -45);
+                    firstClick = false;
+                } else
+                    cal.add(Calendar.DATE, -42);
+
+                adapter.nextMonth(cal.getTime());
+                adapter.notifyDataSetChanged();
+                startDate = cal.getTime();
+            }
+        });
     }
 }
